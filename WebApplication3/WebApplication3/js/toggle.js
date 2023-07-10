c90e46694a996a007bfd8e0eb257a3d4a4e2a0f8
Sneha2151992 <87067044+Sneha2151992@users.noreply.github.com>
2023-07-10 11:11:04 +0530

    new changes adding javascript to have a hamburger menu

---
+++ WebApplication3/WebApplication3/js/toggle.js
@@ -0,0 +1,10 @@
+﻿burger = document.querySelector('.burger');
+myfirstnav = document.querySelector('.myfirstnav');
+myfirstnavlist = document.querySelector('.myfirstnavlist');
+rightnav = document.querySelector('.myfirstrightnav');
+burger.addEventListener('click', () => {
+    myfirstnavlist.classList.toggle('v-nav');
+    rightnav.classList.toggle('v-nav');
+    myfirstnav.classList.toggle('hnav');
+
+})
